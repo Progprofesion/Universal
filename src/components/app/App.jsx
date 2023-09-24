7ab5c0f68137468ec913bc0871061060e2bfe300
Dmitriy <tiemposilencioo@gmail.com>
2023-09-24 11:41:06 +0300

    fixColumsHeight

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import Header from "../header/Header";
 import Interesting from "../interesting/Interesting";
 import Interesting2 from "../interesting/Interesting2";
 import BigBloom from "../bigBloom/BigBloom";
+import Interesting3 from "../interesting/Interesting3";
 import "./app.scss";
 
 const app = () => {
@@ -12,6 +13,7 @@ const app = () => {
       <Interesting />
       <Interesting2 />
       <BigBloom />
+      <Interesting3 />
     </main>
   );
 };
---
+++ src/components/interesting/Interesting3.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Interesting3 = () => {
+  return <section className="interesting3"></section>;
+};
+
+export default Interesting3;
